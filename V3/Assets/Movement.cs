14f4f73aa2e8a22f50e1f96bfe5b4e08fd39942e
Judasznik <judasznikbarabasz@gmail.com>
2024-10-23 21:03:08 +0200

    Add animation and code to activate animation

--- V3/Assets/Movement.cs
+++ V3/Assets/Movement.cs
@@ -6,17 +6,19 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    public float upForce = 100;
+    public float upForce = 1000;
     public float speed = 20;
     public float runSpeed = 100;
     public bool isGrounded = false;
     public bool IsLeftDown = false;
+    public bool IsUpArrowDown = false;
     Animator anim;
     Rigidbody rb;
     SpriteRenderer sr;
     float moveHorizontal;
+    float moveVertical;
 
- 
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -24,12 +26,13 @@ public class Movement : MonoBehaviour
         anim = GetComponentInChildren<Animator>();
     }
 
-   
+
     void Update()
     {
 
         IsLeftDown = Input.GetKey(KeyCode.LeftShift);
-        IsUpDown = Input.GetKey(KeyCode.UpShift);
+        IsUpArrowDown = Input.GetKey(KeyCode.UpArrow);
+        //IsDownArrowDown = Input.GetKey(KeyCode.DownArrow);
         moveHorizontal = Input.GetAxis("Horizontal");
         moveVertical = Input.GetAxis("Vertical");
 
@@ -37,50 +40,69 @@ public class Movement : MonoBehaviour
         {
             rb.AddForce(Vector3.up * upForce);
             isGrounded = false;
+            
         }
 
-        if (moveHorizontal > 0)
+        if (isGrounded == false) { anim.SetBool("IsJumping", true); }
+        else
+        {
+            anim.SetBool("IsJumping", false);
+        }
+            
+            
+            if (moveHorizontal > 0)
         {
             sr.flipX = false;
         }
-        else if(moveHorizontal < 0)
+        else if (moveHorizontal < 0)
         {
             sr.flipX = true;
         }
-        
-        if(moveHorizontal == 0)
-    {
-        anim.SetBool("Is Running", false);
-    }
-    else
-    {
-        anim.SetBool("Is Running", true);
+
+        if (moveHorizontal == 0)
+        {
+            anim.SetBool("Is Running", false);
+        }
+        else
+        {
+            anim.SetBool("Is Running", true);
+        }
+
+      
+
     }
 
-        }
-    
     void FixedUpdate()
     {
         if (IsLeftDown)
         {
             rb.velocity = new Vector3((moveHorizontal) * runSpeed * Time.deltaTime, rb.velocity.y, 0);
         }
-        else 
+        else
         {
             rb.velocity = new Vector3((moveHorizontal) * speed * Time.deltaTime, rb.velocity.y, 0);
         }
 
-        if (IsUpDown)
-        {
-            rb.velocity = new Vector4((moveVertical) * runSpeed * Time.deltaTime, rb.velocity.y, moveVertical * runSpeed);
-        }
-        else
-        {
-            rb.velocity = new Vector4((moveVertical) * speed * Time.deltaTime, rb.velocity.y, moveVertical * runSpeed);
-        }
+        if (IsUpArrowDown)
+                {
+                    rb.velocity = new Vector4(0, rb.velocity.y, (moveVertical) * speed * Time.deltaTime);
+                }
+                else
+                {
+                    rb.velocity = new Vector4(0, rb.velocity.y, (moveVertical) * speed * Time.deltaTime);
 
+                }
+        /*
+                if (IsDownArrowDown)
+                {
+                    rb.velocity = new Vector4(0, rb.velocity.y, (moveHorizontal) * speed * Time.deltaTime);
+                }
+                else
+                {
+                    rb.velocity = new Vector4(0, rb.velocity.y, (moveHorizontal) * speed * Time.deltaTime);
+                }*/
     }
-    
+
 
     private void OnCollisionEnter(Collision collision)
     {
