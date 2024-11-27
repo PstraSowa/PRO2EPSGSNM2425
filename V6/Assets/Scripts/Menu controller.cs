8a65a6e658d2ea1ef7e0b55ec183da454b86ce9c
Judasznik <judasznikbarabasz@gmail.com>
2024-11-27 20:36:33 +0100

    Make continue button interactable and functional

--- V6/Assets/Scripts/Menu controller.cs
+++ V6/Assets/Scripts/Menu controller.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Menucontroller : MonoBehaviour
 {
     public GameObject menuPanel;
     public Movement playermovement;
+    public Button continueButton;
+
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -14,6 +17,19 @@ public class Menucontroller : MonoBehaviour
         }
 
     }
+
+    public void Start()
+    {
+        if (DataSerializer.AnySaves() == false)
+        {
+            continueButton.interactable = false;
+        }
+        else
+        {
+            continueButton.interactable = true;
+        }
+    }
+
     public void NewGame()
     {
         new SaveData();
@@ -23,10 +39,18 @@ public class Menucontroller : MonoBehaviour
     {
         playermovement.Save();
         DataSerializer.Save();
+
+        if (DataSerializer.AnySaves() == true)
+        {
+            continueButton.interactable = true;
+        }
+        
     }
+
     public void Continue()
     {
-
+        DataSerializer.Load();
+        menuPanel.SetActive(!menuPanel.activeSelf);
     }
 
     public void Exit()
